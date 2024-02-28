b2f9b55d90011c66138dacec7648a7af3ecb5d45
vic2ray <vic2ray@qq.com>
2024-02-28 20:33:26 +0800

    0228

--- run_group.py
+++ run_group.py
@@ -31,6 +31,16 @@ def run_group(cl):
         except Exception as e:
             search_input.fill('')
             return False
+
+    def remove_box():
+        # 去除打开消息通知遮罩层
+        try:
+            button_text = '打开'
+            xpath_expression = f"//button[text()='{button_text}']"
+            button = page.wait_for_selector(f"xpath={xpath_expression}", strict=True, timeout=5000)
+            if button: button.click()  # 按钮存在，执行点击操作
+        except Exception as e:
+            pass
         
     proxy = cl.proxy
         
@@ -56,54 +66,12 @@ def run_group(cl):
         context.add_cookies(cookies=get_cookies(cl.sessionid))
         page = context.new_page()
 
-        page.goto('https://www.instagram.com/direct/inbox/', timeout=60000)
+        page.goto('https://www.instagram.com/direct/inbox/', timeout=30000)
+
+        remove_box()
+
         # 等待页面加载完成
-        page.wait_for_load_state('networkidle', timeout=60000)
-
-        # 去除打开消息通知遮罩层
-        try:
-            button_text = '打开'
-            xpath_expression = f"//button[text()='{button_text}']"
-            button = page.wait_for_selector(f"xpath={xpath_expression}", strict=True, timeout=5000)
-            if button: button.click()  # 按钮存在，执行点击操作
-        except Exception as e:
-            pass
-
-        # 查找最后建的群组, 确定最后发送消息时间
-        # threads = cl.direct_threads()
-        # group = None
-        # last_active_at = None
-        # meet_dm_delay = False  # 是否达到时间间隔要求
-        # for thread in threads:
-        #     if thread.is_group and thread.admin_user_ids[0] == cl.user_id:
-        #         group = thread
-        #         break
-        # 存在群组, 查找最近群组的最近20条群组消息, 获取管理员发布消息的最后时间
-        # if group:
-        #     print(group.id, len(group.users), group.thread_title)
-        #     messages = cl.direct_messages(group.id)
-        #     # 如果没有主动发送消息的最后时间, 则取最后一条群组消息的时间
-        #     last_active_at = messages[0].timestamp
-        #     for message in messages:
-        #         if int(message.user_id) == cl.user_id:
-        #             if message.item_type == 'media':
-        #                 print('[图片消息]', message.timestamp.isoformat(), message.media.thumbnail_url)
-        #                 last_active_at = message.timestamp
-        #                 break
-        #             if message.item_type == 'xma_link':
-        #                 print('[文字消息]', message.timestamp.isoformat(), message.text)
-        #                 last_active_at = message.timestamp
-        #                 break
-        #     print('最后管理员消息或群组活跃时间', last_active_at.isoformat())
-        #     timedelta = (datetime.now() - group.messages[0].timestamp).total_seconds() // 60
-        #     if timedelta >= group_dm_delay:
-        #         print('满足设定群组创建和消息间隔, 即将创建新的群组')
-        #         meet_dm_delay = True
-        # 如果最后消息发送时间满足设定间隔(15分钟以上或更久), 则可以创建群组和发送消息
-        # if group and not meet_dm_delay:
-        #     print('不满足设定群组创建和消息间隔, 请等待一段时间后重试')
-        #     return False
-        # else:
+        page.wait_for_load_state('networkidle', timeout=30000)
         for i in range(group_num):
             # 读取用户进行拉群
             # page.goto('https://www.instagram.com/direct/t/7188984047860738')
@@ -112,35 +80,16 @@ def run_group(cl):
                 # 先拉5个人建群
                 try:
                     if button.is_visible() and button.is_enabled():
-                        button.click(timeout=10000)
-                        for i in range(group_round): fill_user(next(users))
-                        page.get_by_text('聊天').click(timeout=30000)
-                        page.wait_for_load_state('networkidle')
-                    else:
-                        print('新建按钮无法点击')
-                        return False
+                        remove_box()
+                    button.click(timeout=10000)
+                    for i in range(group_round): fill_user(next(users))
+                    page.get_by_text('聊天').click(timeout=30000)
+                    page.wait_for_load_state('networkidle')
                 except Exception as e:
                     logger.exception(e)
                     print('群组创建失败!', e)
                     return False
                 print('正在创建群组...')
-                # 拉人
-                # try:
-                #     page.locator(selector='div > svg', has_text="对话信息").click(timeout=30000)
-                #     page.wait_for_timeout(5000)
-                # except Exception as e:
-                #     print('群组创建失败!', e)
-                #     return False
-                # page.get_by_text("添加用户").click(timeout=5000)
-                # page.wait_for_timeout(1000)
-                # for i in range(group_limits // group_round + 1):
-                #     for i in range(group_round): fill_user(next(users))
-                #     page.get_by_text('继续').click(timeout=5000)
-                #     page.wait_for_timeout(1000)
-                #     if i != group_limits // group_round:
-                #         page.get_by_text("添加用户").click(timeout=5000)
-                #         page.wait_for_timeout(1000)
-                # page.keyboard.press('Escape')
                 # 发消息
                 try:
                     if message_button := page.locator('xpath=//div[@aria-label="发消息"]/p'):
